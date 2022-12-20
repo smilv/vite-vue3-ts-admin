9aea7d358746fdc3ee282931a8c4ff3d3f186fc1
smilv <owou@icloud.com>
2022-12-20 16:34:33 +0800

    更改项目

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// import "~/styles/element/index.scss";
+
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+
+import "~/styles/index.scss";
+import "uno.css";
+
+// If you want to use ElMessage, import it.
+import "element-plus/theme-chalk/src/message.scss";
+
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount("#app");
