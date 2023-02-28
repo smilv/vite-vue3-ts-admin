987cb60f7ec112150051f69c4fbd9ca2ccc1aad9
smilv <owou@icloud.com>
2023-02-28 15:04:53 +0800

    package 升级

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,11 @@
  * @Author: zhaobin
  * @Date: 2022-08-13 16:22:58
  * @LastEditors: zhaobin
- * @LastEditTime: 2023-01-13 15:38:30
+ * @LastEditTime: 2023-02-28 14:47:44
  */
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
-
-// import "@/styles/element/index.scss";
-
-// import ElementPlus from "element-plus";
-// import "element-plus/dist/index.css";
 
 import "@/styles/index.scss";
 import "uno.css";
@@ -21,6 +16,5 @@ import "uno.css";
 import "element-plus/theme-chalk/src/message.scss";
 
 const app = createApp(App);
-// app.use(ElementPlus);
 app.use(router);
 app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,13 @@
  * @Author: zhaobin
  * @Date: 2022-12-27 17:09:18
  * @LastEditors: zhaobin
- * @LastEditTime: 2023-01-13 15:38:01
+ * @LastEditTime: 2023-02-28 14:17:03
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Layout from "@/layout/index.vue";
 
 // import.meta.globEager() 直接引入所有的模块 Vite 独有的功能
-const modules = import.meta.globEager("./modules/**/*.ts");
+const modules:any = import.meta.globEager("./modules/**/*.ts");
 const routeModuleList: RouteRecordRaw[] = [];
 
 // 加入到路由集合中
